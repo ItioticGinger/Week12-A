c10d8c13ce8d0a6f36e1f63f0dc7c9ce9e927e05
ItioticGinger <matthewbrause@mymail.mines.edu>
2019-11-21 10:18:54 -0700

    added FindWordCount

--- Week12-utility.py
+++ Week12-utility.py
@@ -16,3 +16,17 @@ def UpdateString(string, update, num):
     for i in string_list:
         string_updated += i
     return string_updated
+
+def FindWordCount(text_list, string_test):
+    count = 0
+    for i in text_list:
+        if string_test in i:
+            for j in range(len(i)-len(string_test)):
+                if string_test[0] == i[j]:
+                    bad = 0
+                    for k in range(len(string_test)):
+                        if i[j] != string_test[k]:
+                            bad = 1
+                    if bad == 0:
+                        count += 1
+    return count
